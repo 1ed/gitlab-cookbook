6cb0fbbe1e2bafa2bbb084fad6555cb7c360b640
Gábor Egyed <egyed.gabor@mentha.hu>
2013-05-20 18:13:04 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# gitlab cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Gábor Egyed (<egyed.gabor@mentha.hu>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "gitlab"
+maintainer       "Gábor Egyed"
+maintainer_email "egyed.gabor@mentha.hu"
+license          "MIT"
+description      "Installs/Configures gitlab"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
