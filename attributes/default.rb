780473a6a4c0dc5746385abec01c5362fda8972e
Gábor Egyed <egyed.gabor@mentha.hu>
2013-05-25 14:16:28 +0200

    updated attributes file

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,31 +4,15 @@
 #
 # Copyright (C) 2013 Gábor Egyed
 #
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
 #
 
 default[:gitlab][:host] = node[:fqdn]
 default[:gitlab][:user] = "git"
 default[:gitlab][:user_home] = "/home/#{node[:gitlab][:user]}"
 default[:gitlab][:packages] = %w{
-  curl checkinstall ruby1.9.3 git-core openssh-server openssl
+  curl checkinstall ruby1.9.3 openssh-server openssl
   build-essential ruby1.9.1-dev zlib1g-dev libyaml-dev libssl-dev
   libgdbm-dev libreadline-dev libncurses5-dev libffi-dev libxml2-dev
   libxslt1-dev libcurl4-openssl-dev libicu-dev libc6-dev
@@ -61,8 +45,3 @@ default[:gitlab_shell][:git_url] = "https://github.com/gitlabhq/gitlab-shell.git
 default[:gitlab_shell][:git_ref] = "v1.4.0"
 default[:gitlab_shell][:repositories] = "#{node[:gitlab][:user_home]}/repositories"
 default[:gitlab_shell][:redis_config] = nil
-#{
-#  :bin => "/usr/local/bin/redis-cli",
-#  :socket => "/var/run/redis/master/redis_master.sock",
-#  :namespace => "resque:gitlab"
-#}
