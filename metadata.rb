73372742ae98675bcebb99775647fd43e2327b9d
Gábor Egyed <egyed.gabor@mentha.hu>
2013-10-29 22:08:15 +0100

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "egyed.gabor@mentha.hu"
 license          "MIT"
 description      "Installs/Configures gitlab"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 
 supports "ubuntu"
 
