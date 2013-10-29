10f49f3d29440a41a56442dca22b487229e60ec1
Gábor Egyed <egyed.gabor@mentha.hu>
2013-10-29 21:40:09 +0100

    release 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "egyed.gabor@mentha.hu"
 license          "MIT"
 description      "Installs/Configures gitlab"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "1.0.0"
 
 supports "ubuntu"
 
